47e37e7721cbbb498eab4dd61fbc984ff9fd226d
Daria Gonsalves <daria.gonsalves@hyuna.bb>
2018-09-04 17:30:10 -0400

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user')
-const {ObjectID} = require('mongodb');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -36,21 +37,19 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
-   return res.status(404).send();
- }
- Todo.findById(id).then((todo) =>{
-   if(!todo){
-     return res.status(404).send();
-   }
-   res.send({todo});
- }.catch((e) => {
-   res.status(400).send();
- });
-//  //OR .catch((e) => {
-//  res.status(400).send();
-// });
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 
